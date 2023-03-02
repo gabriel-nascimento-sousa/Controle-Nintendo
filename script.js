19d6678ac01a277a9c6f66f6e54dff4f83808c89
gabriel <sousa.gabriel.dev@gmail.com>
2023-03-02 20:41:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var fundo = document.getElementById('fundo');
+fundo.style.transition = '1s'
+
+function Red(){
+    fundo.style.backgroundColor = 'red'
+}
+function Blue(){
+    fundo.style.backgroundColor = 'blue'
+}
+function Green(){
+    fundo.style.backgroundColor = 'green'
+}
+function yellow(){
+    fundo.style.backgroundColor = 'yellow'
+}
